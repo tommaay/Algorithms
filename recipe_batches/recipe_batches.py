3766378a3b63a8e897b8b41a28f7717eb170bd3f
ToMMaaY <thuynh629@gmail.com>
2019-03-25 15:50:43 -0700

    completed recipe_batches.py

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,23 @@
 
 import math
 
-def recipe_batches(recipe, ingredients):
-  pass 
 
+def recipe_batches(recipe, ingredients):
+    batch = {}
+    for k, v in recipe.items():
+        if k not in ingredients:
+            return 0
+        batch[k] = ingredients[k] // v
+    return min(batch.values())
+
+
+print(recipe_batches({'milk': 100, 'butter': 50, 'cheese': 10}, {
+      'milk': 198, 'butter': 52, 'cheese': 10}))
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
